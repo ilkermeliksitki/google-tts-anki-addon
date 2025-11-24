e2dc56690c447bd92c9f1afb9c599db7d22be540
ilker m. sitki <ilkermeliksitki@proton.me>
2025-11-24 01:21:12 +0100

    refactor main module and remove hardcoded logic
    Changes to be committed:
          modified:   src/google_tts_anki_addon/main.py

--- src/google_tts_anki_addon/main.py
+++ src/google_tts_anki_addon/main.py
@@ -1,24 +1,17 @@
 from anki.media import MediaManager
-import time
 from aqt import mw
 from aqt.sound import av_player
-from aqt.utils import showInfo, qconnect, showText
-# from aqt.qt import QKeySequence, QAction
+from aqt.utils import showInfo, qconnect, showText, tooltip, getText
 from PyQt6.QtGui import QAction, QKeySequence
-from PyQt6.QtWidgets import QMenu
-from aqt.utils import tooltip, getText
-
-from anki.template import TemplateRenderContext, TemplateRenderOutput
-from anki import hooks
 
 from .pronunciation import create_sound_file, config
 
 
-def rreplace(s:str, old:str, new:str, max_replace=1):
+def rreplace(s: str, old: str, new: str, max_replace=1):
     """
-    this function work from the opposite direction of str.replace function.
-    >>> rreplace("mississipi", "p", "P", 1)
-    'mississiPi'
+    this function works from the opposite direction of str.replace function.
+    >>> rreplace("mississippi", "p", "P", 1)
+    'mississipPi'
     """
     ls = s.rsplit(old, max_replace)
     return new.join(ls)
@@ -27,20 +20,34 @@ def rreplace(s:str, old:str, new:str, max_replace=1):
 def append_sound_to_note(text, filename):
     """
     append sound file to the last field of the card.
-    If the text of the sound is found in that field, it will only append the play button in front of it.
+    if the text of the sound is found in that field,
+    it will only append the play button in front of it.
     """
+    # ensure we are in a state where we can access the note
+    if mw.reviewer.card is None:
+        showText("The sound did not append to any note. You must be in review mode.")
+        return
+
     try:
         note = mw.reviewer.card.note()
+        if note is None:
+             showText("The sound did not append to any note. Note is None.")
+             return
+
         ls_tp = note.items()
+        if not ls_tp:
+             showText("The sound did not append to any note. Note has no fields.")
+             return
+
         first_field_name = ls_tp[0][0]
-        first_field_content= ls_tp[0][1]
+        first_field_content = ls_tp[0][1]
         last_field_name = ls_tp[-1][0]
         last_field_content = ls_tp[-1][1]
 
         state = mw.reviewer.state
         if state == "question":
             # the sound will be added to the question side at the end without writing the sound contents
-            # this is done because of the pronunciation of the words puts at the end of first field.
+            # this is done because the pronunciation of the words puts at the end of first field.
             note[first_field_name] = first_field_content + f" [sound:{filename}]"
         else:
             # this part is for answer state.
@@ -48,13 +55,13 @@ def append_sound_to_note(text, filename):
                 note[last_field_name] = rreplace(last_field_content, text, f"{text} [sound:{filename}]")
             else:
                 note[last_field_name] = last_field_content + f"\n<br>\n{text} [sound:{filename}]"
+
         mw.col.update_note(note)
         mw.reviewer._redraw_current_card()
         tooltip("Sound is added.", 500)
-    except AttributeError:
-        showText(
-            "The sound did not append to any note. Because you have to be in review mode"
-        )
+
+    except Exception as e:
+        showText(f"An error occurred while appending sound: {e}")
 
 
 def append_sound_to_media_file(desired_name, binary_text):
@@ -66,22 +73,22 @@ def append_sound_to_media_file(desired_name, binary_text):
 def prompt_search():
     text, number = getText("Enter a text:", title="Pronunciation from Google-API")
     text = text.strip()
+
     if len(text) > 1 and number == 1:
-        """number 1 means, user click to okay."""
-        if text.endswith("~~"):
-            text = text[:-2] + " jemand oder etwas Akkusativ"
-        elif text.endswith("~"):
-            text = text[:-1] + " etwas Akkusativ"
-        binary_data = create_sound_file(text)
-        filename = append_sound_to_media_file(desired_name="google_api.mp3", binary_text=binary_data)
-        # tooltip("Playing...", period=1000)
-        av_player.play_file(filename)
-        append_sound_to_note(text, filename)
+        # number 1 means, user clicked okay.
+        try:
+            binary_data = create_sound_file(text)
+            filename = append_sound_to_media_file(desired_name="google_api.mp3", binary_text=binary_data)
+            av_player.play_file(filename)
+            append_sound_to_note(text, filename)
+        except Exception as e:
+            showInfo(f"Failed to create or play sound: {e}")
+
     elif number == 0:
-        """number 0 means, user click cancel"""
+        # number 0 means, user clicked cancel
         pass
     else:
-        showInfo("Text should be grater than one letter.")
+        showInfo("Text should be greater than one letter.")
 
 
 action = QAction("&Google-TTS API", mw)
